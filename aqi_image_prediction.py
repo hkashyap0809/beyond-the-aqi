5e7c2bec708ad6d9d800985420fe369a207be291
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2024-04-17 01:13:58 +0530

    Added Prediction Model

--- aqi_image_prediction.py
+++ aqi_image_prediction.py
@@ -1,4 +1,34 @@
 import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+import matplotlib.pyplot as plt
+import numpy as np
+
+def predict():
+    imgPath = st.session_state['uploaded_file']
+    print(imgPath)
+    model = load_model('./models/model_inception.h5')
+    img = load_img(imgPath, target_size=(224, 224))
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    p = np.argmax(model.predict(x))
+    if p == 0:
+        st.write("Irrelevant data")
+    elif p == 1:
+        st.write("Moderate pollution")
+    elif p == 2:
+        st.write("No pollution")
+    elif p == 3:
+        st.write("Severe pollution")
 
 def aqi_image_prediction_component():
     st.title("Image Prediction")
+    uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False)
+    if uploaded_file is not None:
+        st.session_state['uploaded_file'] = uploaded_file
+    if "uploaded_file" in st.session_state:
+        st.image(uploaded_file)
+        predict()
--- main-app.py
+++ main-app.py
@@ -4,14 +4,16 @@ from aqi_data import aqi_data_component
 from aqi_gpt import aqi_gpt_component
 from aqi_image_prediction import aqi_image_prediction_component
 
+def main():
+    st.title("BEYOND THE AQI")
+    tab1, tab2, tab3 = st.tabs(["AQI DATA", "AQI GPT", "IMAGE PREDICTION"])
+    with tab1:
+        aqi_data_component()
+    with tab2:
+        aqi_gpt_component()
+    with tab3:
+        aqi_image_prediction_component()
 
 
-if st.sidebar.button("AQI DATA"):
-    aqi_data_component()  
-
-if st.sidebar.button("AQI GPT"):
-    aqi_gpt_component()
-
-
-if st.sidebar.button("Image prediction"):
-    aqi_image_prediction_component()
+if __name__ == "__main__":
+    main()
